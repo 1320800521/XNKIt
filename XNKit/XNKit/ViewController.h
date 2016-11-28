6d7d3e4a7361f440dfafeb4e85b4b5201751347e
hah <pengfei20140701@126.com>
2016-11-28 18:31:37 +0800

    完善 视频播放器，进度条，时间等

---
+++ XNKit/XNKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XNKit
+//
+//  Created by 小鸟 on 2016/11/28.
+//  Copyright © 2016年 小鸟. All rights reserved.
+//
+
+#import "XNViewController.h"
+
+@interface ViewController : XNViewController
+
+@end
---
+++ XNKit/XNKit/XNApp/NXlaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  NXlaunchViewController.h
+//  XNKit
+//
+//  Created by 小鸟 on 2016/11/28.
+//  Copyright © 2016年 小鸟. All rights reserved.
+//
+
+#import "XNViewController.h"
+
+@interface NXlaunchViewController : XNViewController
+
+@end
