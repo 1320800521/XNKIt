748f4961484158459cfdff0d9e099896a3f8b2ad
hah <pengfei20140701@126.com>
2016-12-07 16:56:32 +0800

    新建网络文件，但是没有写网络请求

---
+++ XNKit/XNKit/XNApp/network/XNNetWork.h
@@ -0,0 +1,13 @@
+//
+//  XNNetWork.h
+//  XNKit
+//
+//  Created by 小鸟 on 2016/12/1.
+//  Copyright © 2016年 小鸟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XNNetWork : NSObject
+
+@end
---
+++ XNKit/XNKit/XNApp/network/XNNetWork.m
@@ -0,0 +1,13 @@
+//
+//  XNNetWork.m
+//  XNKit
+//
+//  Created by 小鸟 on 2016/12/1.
+//  Copyright © 2016年 小鸟. All rights reserved.
+//
+
+#import "XNNetWork.h"
+
+@implementation XNNetWork
+
+@end
