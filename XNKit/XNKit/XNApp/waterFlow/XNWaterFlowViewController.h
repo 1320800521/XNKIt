12eef2d61eecb3d1dbcc1020f92798125dfe43b4
hah <pengfei20140701@126.com>
2017-01-06 16:18:48 +0800

    修改播放器

---
+++ XNKit/XNKit/XNApp/waterFlow/XNWaterFlowViewController.h
@@ -0,0 +1,13 @@
+//
+//  XNWaterFlowViewController.h
+//  XNKit
+//
+//  Created by 小鸟 on 2017/1/4.
+//  Copyright © 2017年 小鸟. All rights reserved.
+//
+
+#import "XNViewController.h"
+
+@interface XNWaterFlowViewController : XNViewController
+
+@end
